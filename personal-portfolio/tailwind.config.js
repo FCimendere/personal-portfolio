6688f828f61fbc242482ae2481fb3c71980301d6
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-12-06 15:41:23 +0100

    set up project

---
+++ personal-portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "light-grey": "#edf6f9",
+      "light-blue": "#83c5be",
+      "mid-blue": "#006d77",
+    },
+  },
+  plugins: [],
+};
