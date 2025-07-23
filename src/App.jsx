7f01b0970fc6f4371cef5176840db2e0c4c38453
Fulya Cimendere <fulyacoskun.87@gmail.com>
2025-07-23 23:04:55 +0200

    add parallax effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [activeId, setActiveId] = useState("");
+
   useEffect(() => {
     localStorage.setItem("theme", "light");
     const selectedTheme = localStorage.getItem("theme");
@@ -24,16 +25,17 @@ function App() {
 
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-display md:px-12 md:py-16 lg:py-0">
-      <div className="lg:flex lg:justify-between lg:gap-4">
+      <div className="lg:flex lg:gap-0">
         {/* bg-light-grey flex px-6 py-12 lg:gap-4 */}
-        <div className="left-section lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
+        <div className="left-section lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
           {/* left-section flex-1 max-h-screen sticky top-0 overflow-y-auto */}
           <Home activeId={activeId} setActiveId={setActiveId} />
         </div>
-        <div className="right-section pt-24 lg:w-[52%] lg:py-24">
+        <div className="right-section pt-24 lg:w-1/2 lg:py-24">
           {/* flex-1 */}
           {/* <SplashCursor /> */}
-          <RightSection onSectionChange={handleSectionChange} />
+          {/* <RightSection onSectionChange={handleSectionChange} /> */}
+          <RightSection activeId={activeId} onSectionChange={setActiveId} />
         </div>
       </div>
     </div>
--- src/pages/RightSection/RightSection.jsx
+++ src/pages/RightSection/RightSection.jsx
@@ -3,14 +3,26 @@ import About from "../About/About";
 import Experience from "../Experience/Experience";
 import Project from "../Projects/Project";
 import Contact from "../Contacts/Contact";
-import { useIntersectionObserver } from "../../utils/useIntersectionObserver";
+// import { useIntersectionObserver } from "../../utils/useIntersectionObserver";
 import GoToTop from "../../components/GoToTop";
 import DarkButton from "../../components/DarkButton";
 import { throttle } from "lodash";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import cloudy from "../../assets/cloudy.png";
+import hill from "../../assets/hill.png";
+import balloon from "../../assets/balloon.png";
+import clouds from "../../assets/clouds.png";
 
-const RightSection = ({ onSectionChange }) => {
+const sectionMap = {
+  "link-about": 0,
+  "link-project": 1,
+  "link-experience": 2,
+  "link-contact": 3,
+};
+
+const RightSection = ({ activeId }) => {
   const sections = ["about", "project", "experience", "contact"];
-  const activeSection = useIntersectionObserver(sections);
+  // const activeSection = useIntersectionObserver(sections);
   const [showButton, setShowButton] = useState(false);
   const [isDark, setIsDark] = useState(() => {
     if (typeof window !== "undefined") {
@@ -24,6 +36,7 @@ const RightSection = ({ onSectionChange }) => {
     }
     return false;
   });
+  const parallaxRef = useRef(null);
 
   useEffect(() => {
     const handleScrollButtonVisibility = throttle(() => {
@@ -37,14 +50,19 @@ const RightSection = ({ onSectionChange }) => {
     };
   }, []);
 
-  useEffect(() => {
-    if (activeSection) {
-      onSectionChange(`link-${activeSection}`);
-    }
-  }, [activeSection, onSectionChange]);
+  // useEffect(() => {
+  //   if (activeSection) {
+  //     onSectionChange(`link-${activeSection}`);
+  //   }
+  // }, [activeSection, onSectionChange]);
 
   useEffect(() => {
-    // Update the DOM when dark mode changes
+    if (activeId && sectionMap.hasOwnProperty(activeId)) {
+      parallaxRef.current.scrollTo(sectionMap[activeId]);
+    }
+  }, [activeId]);
+
+  useEffect(() => {
     if (isDark) {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
@@ -53,6 +71,32 @@ const RightSection = ({ onSectionChange }) => {
       localStorage.setItem("theme", "light");
     }
   }, [isDark]);
+
+  // useEffect(() => {
+  //   const handleParallaxScroll = () => {
+  //     if (!parallaxRef.current) return;
+
+  //     const container = parallaxRef.current.container;
+  //     const scrollY = container.scrollTop;
+  //     const pageHeight = window.innerHeight;
+
+  //     const currentPage = Math.round(scrollY / pageHeight);
+  //     const newActiveId = Object.entries(sectionMap).find(
+  //       ([, value]) => value === currentPage
+  //     )?.[0];
+
+  //     if (newActiveId) {
+  //       onSectionChange?.(newActiveId);
+  //     }
+  //   };
+
+  //   const container = parallaxRef.current?.container;
+  //   container?.addEventListener("scroll", handleParallaxScroll);
+
+  //   return () => {
+  //     container?.removeEventListener("scroll", handleParallaxScroll);
+  //   };
+  // }, [onSectionChange]);
 
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -66,7 +110,7 @@ const RightSection = ({ onSectionChange }) => {
     <>
       <DarkButton switchMode={darkModeHandler} isDark={!isDark} />
 
-      <div className="space-y-16">
+      {/* <div className="space-y-16">
         <section id="about">
           <About />
         </section>
@@ -85,7 +129,128 @@ const RightSection = ({ onSectionChange }) => {
             scrollUp={handleScrollToTop}
           />
         )}
+      </div> */}
+      <div style={{ height: "100vh" }} className="relative">
+        <Parallax ref={parallaxRef} pages={4}>
+          <ParallaxLayer offset={0} speed={0.5}>
+            <section id="about" className="min-h-screen">
+              <About />
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={0.9}
+            speed={-0.3}
+            style={{ pointerEvents: "none" }}
+          >
+            <img
+              src={balloon}
+              style={{
+                display: "block",
+                width: "15%",
+                marginLeft: "70%",
+                opacity: 0.5,
+              }}
+            />
+            <img
+              src={clouds}
+              style={{
+                display: "block",
+                width: "15%",
+                marginLeft: "20%",
+                opacity: 0.5,
+              }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1} speed={0.5}>
+            <section id="project" className="min-h-screen">
+              <Project />
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1.7}
+            speed={-0.3}
+            style={{ pointerEvents: "none" }}
+          >
+            <img
+              src={cloudy}
+              style={{
+                display: "block",
+                width: "20%",
+                marginLeft: "20%",
+                opacity: 0.5,
+              }}
+            />
+            <img
+              src={cloudy}
+              style={{
+                display: "block",
+                width: "15%",
+                marginLeft: "60%",
+                opacity: 0.5,
+              }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={2} speed={0.5}>
+            <section id="experience" className="min-h-screen">
+              <Experience />
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={2.9}
+            speed={0.5}
+            style={{ pointerEvents: "none" }}
+          >
+            <img
+              src={balloon}
+              style={{
+                display: "block",
+                width: "15%",
+                marginLeft: "80%",
+                opacity: 0.8,
+              }}
+            />
+            <img
+              src={clouds}
+              style={{
+                display: "block",
+                width: "15%",
+                marginLeft: "20%",
+                opacity: 0.5,
+              }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={3} speed={0.5}>
+            <section id="contact" className="min-h-screen">
+              <Contact />
+            </section>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={3.4}
+            speed={-0.4}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              pointerEvents: "none",
+            }}
+          >
+            <img src={hill} style={{ width: "60%", opacity: 0.7 }} />
+          </ParallaxLayer>
+        </Parallax>
       </div>
+      {showButton && (
+        <GoToTop
+          showGoTop="fixed bottom-5 right-7 z-50 cursor-pointer p-4 overflow-hidden"
+          scrollUp={handleScrollToTop}
+        />
+      )}
     </>
   );
 };
